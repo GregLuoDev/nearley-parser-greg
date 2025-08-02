4f850f74b7f12918871661bbf6d7061291a1b6cd
Greg Luo <greg.luo@outlook.com>
2025-08-02 23:10:45 +1000

    tiny updates

--- src/parser/components/ParseAST.tsx
+++ src/parser/components/ParseAST.tsx
@@ -4,7 +4,7 @@ export function ParseAST({ ast }: { ast: IAst | null }) {
   return (
     <>
       {ast && (
-        <pre className="mt-6 bg-gray-100 rounded text-sm overflow-x-auto">
+        <pre className="mt-6 bg-gray-100 rounded text-sm overflow-x-auto text-black">
           <b>AST:</b>{" "}
           <div data-testid="ast">{JSON.stringify(ast, null, 4)}</div>
         </pre>
